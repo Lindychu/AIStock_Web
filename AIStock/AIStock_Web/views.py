56f45b48e15ca0f5d89f138080b97b77a4512df1
Lindyc <elu903@gmail.com>
2022-09-03 21:57:43 +0800

    first

---
+++ AIStock/AIStock_Web/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+# Create your views here.
+def test(request):
+    # if 'e_id' not in request.session:
+        
+    context = {
+    }
+    return render(request, 'test.html', {})
